9fc6267c33f83960a5ca83e4621e62e386d369d6
Lucas Kramer <krame505@umn.edu>
2015-12-19 16:07:47 -0600

    Added cutoff to minmax, currently dissabled as not very good

--- minmaxsearch.py
+++ minmaxsearch.py
@@ -12,11 +12,11 @@ def minmax(state, depth, score, heuristic, terminal, player1, player2, paralleli
 
     # Call max_value if no parallelization
     if not parallelize:
-        return max_value(state, float('-inf'), float('inf'), depth, score, heuristic, terminal, player1, player2)
+        return max_value(state, state, float('-inf'), float('inf'), depth, score, heuristic, terminal, player1, player2)
 
     # Search base case
     opt = Move(-1, -1, -1, -1, [], "No move:\n" + str(state))
-    if depth <= 0 or terminal(state):
+    if terminal(state, state, depth):
         return score(state), opt
 
     # Initialize the parallel process pool
@@ -38,7 +38,7 @@ def minmax(state, depth, score, heuristic, terminal, player1, player2, paralleli
     # in the outputs queue
     def submit_next():
         s, move = inputs.get()
-        outputs.put((pool.apply_async(min_value, args = (state.move(move), alpha, beta, depth - 1, score, heuristic, terminal, player1, player2)), move))
+        outputs.put((pool.apply_async(min_value, args = (state.move(move), state, alpha, beta, depth - 1, score, heuristic, terminal, player1, player2)), move))
 
     # Retrieves a result handle from the outputs queue, waits for it to finish, and returns the score and move
     def get_next():
@@ -71,19 +71,19 @@ def minmax(state, depth, score, heuristic, terminal, player1, player2, paralleli
 
     return v, opt
 
-def max_value(state, alpha, beta, depth, score, heuristic, terminal, player1, player2):
+def max_value(state, initial_state, alpha, beta, depth, score, heuristic, terminal, player1, player2):
     """Max portion of minmax search"""
 
     # Search base case
     opt = Move(-1, -1, -1, -1, [], "No move:\n" + str(state))
-    if depth <= 0 or terminal(state):
+    if terminal(state, initial_state, depth):
         return score(state), opt
 
     v = float('-inf')
     # Iterate through moves in sorted order according to the heuristic
     for s, move in sorted([(heuristic(state.move(m), depth), m) for m in state.moves(player1)]):
         # Compute the next min value, check if it is optimal
-        s1, m = min_value(state.move(move), alpha, beta, depth - 1, score, heuristic, terminal, player1, player2)
+        s1, m = min_value(state.move(move), initial_state, alpha, beta, depth - 1, score, heuristic, terminal, player1, player2)
         v, opt = max((v, opt), (s1, move))
         # Check for beta cutoff and update alpha
         if v >= beta:
@@ -91,18 +91,18 @@ def max_value(state, alpha, beta, depth, score, heuristic, terminal, player1, pl
         alpha = max(alpha, v)
     return v, opt
 
-def min_value(state, alpha, beta, depth, score, heuristic, terminal, player1, player2):
+def min_value(state, initial_state, alpha, beta, depth, score, heuristic, terminal, player1, player2):
     """Min portion of minmax search"""
     # Search base case
     opt = Move(-1, -1, -1, -1, [], "No move:\n" + str(state))
-    if depth <= 0 or terminal(state):
+    if terminal(state, initial_state, depth):
         return score(state), opt
 
     v = float('inf')
     # Iterate through moves in sorted order according to the heuristic
     for s, move in sorted([(heuristic(state.move(m), depth), m) for m in state.moves(player2)], reverse = True):
         # Compute the next max value, check if it is optimal
-        s1, m = max_value(state.move(move), alpha, beta, depth - 1, score, heuristic, terminal, player1, player2)
+        s1, m = max_value(state.move(move), initial_state, alpha, beta, depth - 1, score, heuristic, terminal, player1, player2)
         # Check for alpha cutoff and update beta
         v, opt = min((v, opt), (s1, move))
         if v <= alpha:
@@ -132,7 +132,7 @@ class MinMaxSearchPlayer(Player):
         slower one further down where it doesn't matter.  By default, it is just the difference in the
         number of pieces each player has"""
         return len(board.active_pieces(self.num)) - len(board.active_pieces(self.opponent))
-
+        
     def calc_score(self, board):
         """Wrapper for score function that adds checks for immediate win/loss and randomization"""
         if board.game.lost(board, self.num):
@@ -142,9 +142,28 @@ class MinMaxSearchPlayer(Player):
         else:
             return self.score(board) + random.randint(-self.randomize, self.randomize)
 
-    def terminal(self, board):
+    def cutoff(self, board, initial_board, depth, threshold = 2):
+        """Cutoff test function, can be overridden.  By default, it just checks if the score has
+        changed by a threshold"""
+        initial_score = self.calc_score(initial_board) 
+        current_score = self.calc_score(board) 
+        return (initial_score == float('-inf') or
+                initial_score == float('inf') or
+                current_score == float('-inf') or
+                current_score == float('inf') or
+                (depth >= 2 and
+                 (initial_score - current_score > threshold or
+                  initial_score - current_score < -threshold)))
+
+    def terminal(self, board, initial_board, depth):
         """Checks if the board's game has been won"""
-        return board.game.lost(board, self.num) or board.game.lost(board, self.opponent)
+        cutoff = False#self.cutoff(board, initial_board, depth)
+        if cutoff:
+            print("Cutoff")
+        return (depth <= 0 or
+                cutoff or
+                board.game.lost(board, self.num) or
+                board.game.lost(board, self.opponent))
 
     def get_move(self, board, verbose = False):
         """Implements alpha-beta search"""
