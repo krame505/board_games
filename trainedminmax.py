86a9c19e3fe285febc4c10a112e3cd10f0a1536e
Lucas Kramer <krame505@umn.edu>
2015-12-27 19:08:14 -0600

    Attempt at pickling sklearn svm class

--- trainedminmax.py
+++ trainedminmax.py
@@ -48,6 +48,7 @@ class TrainedMinMaxSearchPlayer(MinMaxSearchPlayer):
         # If the provided svm is a string ending with '.pickle', then load it from that file
         if isinstance(scoring_svm, str) and scoring_svm.endswith('.pickle'):
             print("Loading svm...")
+            svm = pickle.load(open('sklearn.pickle', 'rb'))
             scoring_svm = pickle.load(open(scoring_svm, 'rb'))
         # If the provided svm is a string ending with '.json', then load training data from
         # that file and train a new SVM.  Dump the generated SVM to svm.pickle
@@ -57,6 +58,7 @@ class TrainedMinMaxSearchPlayer(MinMaxSearchPlayer):
             training_data = json.loads(open(scoring_svm).read())
             scoring_svm = svm.SVR()
             scoring_svm.fit(*training_data)
+            pickle.dump(svm, open('sklearn.pickle', 'wb'))
             pickle.dump(scoring_svm, open('svm.pickle', 'wb'))
 
         self.scoring_svm = scoring_svm
