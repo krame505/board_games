cc75ff2f87900fc519af12f50ffbe48994c05ef9
Lucas Kramer <krame505@umn.edu>
2015-12-29 18:23:54 -0600

    Simplified monte carlo tree search, uses leaf parallelization and upper confidence bound selection

--- montecarlosearch.py
+++ montecarlosearch.py
@@ -3,34 +3,29 @@ from player import *
 from randomized import *
 
 import random
+from math import sqrt, log
 import concurrent.futures
 import time
 
-def run_trial(init_board, init_turn, games = 5):
+def run_trial(init_board, turn):
     """Play a trial game starting from the given board"""
     game = init_board.game
     #player = HeuristicMinMaxSearchPlayer(1, 'default', 5, False, False)
     player = RandomizedPlayer()
     moves = []
-    result = 0
-    for i in range(games):
-        turn = init_turn % 2 + 1
-        board = init_board
-        moves.append([])
-        while not board.game.lost(board, turn) and len(board.moves(turn)) != 0:
-            # Get a move and update the board
-            player.num = turn
-            move = player.get_move(board)
-            moves[-1].append(move)
-            board = board.move(move)
-            # Advance the turn
-            turn = turn % 2 + 1
-        if turn == 2:
-            result += 1
-    return result, moves
+    board = init_board
+    while not board.game.lost(board, turn) and len(board.moves(turn)) != 0:
+        # Get a move and update the board
+        player.num = turn
+        move = player.get_move(board)
+        moves.append(move)
+        board = board.move(move)
+        # Advance the turn
+        turn = turn % 2 + 1
+    return turn, moves
 
 class MonteCarloTree:
-    def __init__(self, move_state, parent = None, depth = 0, player_num = None):
+    def __init__(self, move_state, parent = None, depth = 0, bias = 0.5, player_num = None):
         self.depth = depth
         self.parent = parent
         self.num_runs = 0
@@ -39,13 +34,13 @@ class MonteCarloTree:
             self.state = parent.state.move(move_state)
             self.move = move_state
             self.executor = parent.executor
-            self.results = parent.results
             self.player_num = parent.player_num % 2 + 1
+            self.bias = parent.bias
         else:
             self.state = move_state
             self.executor = concurrent.futures.ProcessPoolExecutor(max_workers = 20)
-            self.results = {}
             self.player_num = player_num
+            self.bias = bias
         self.children = []
 
     def __del__(self):
@@ -53,6 +48,7 @@ class MonteCarloTree:
             self.executor.shutdown()
 
     def __str__(self):
+        depth_limit = 1
         return ("\t" * self.depth +
                 str(self.num_wins) + "/" +
                 str(self.num_runs) + 
@@ -60,7 +56,7 @@ class MonteCarloTree:
                  if self.num_runs > 0
                  else "") +
                 ("\n" + "".join(map(str, self.children))
-                 if self.depth < 3
+                 if self.depth < depth_limit
                  else "<children not shown>\n"))
 
     def __repr__(self):
@@ -69,84 +65,53 @@ class MonteCarloTree:
     def __lt__(self, other):
         return random.choice([True, False]) # TODO, order nodes with same scores
 
-    def run_trials(self, timeout = 10, max_parallel = 10, num_games = 7):
+    def run_trials(self, timeout = 10, num_games = 20):
         start = time.time()
         while time.time() - start < timeout:
-            for node in self.results.keys():
-                if not node.busy():
-                    node.wait()
-                    break
-            if len(self.results) > max_parallel:
-                self.wait_one()
             node = self.select()
-            node.trial(num_games)
-        self.wait_all()
-
-    def wait_one(self):
-        list(self.results.keys())[0].wait()
-
-    def wait_all(self):
-        for node in list(self.results.keys()):
-            node.wait()
-
-    def wait(self):
-        result = self.results[self]
-        self.update(result.result())
-        del self.results[self]
-
-    def busy(self):
-        return self in self.results and not self.results[self].done()
-
-    def all_busy(self):
-        return self.busy() or all(node.busy() for node in self.children)
+            if node:
+                node.trial(num_games)
+            else:
+                print("Search exaustive")
+                break
 
     def update(self, result):
-        wins, movess = result
-        for moves in movess:
-            node = self
-            for move in moves:
-                prev_node = node
-                node = MonteCarloTree(move, prev_node, prev_node.depth + 1)
-                prev_node.children.append(node)
-        node.backpropagate(wins, len(movess))
+        winner, moves = result
+        node = self
+        for move in moves:
+            prev_node = node
+            node = MonteCarloTree(move, prev_node, prev_node.depth + 1)
+            prev_node.children.append(node)
+        node.backpropagate(winner)
         
-    def backpropagate(self, wins, games):
-        self.num_wins += wins
-        self.num_runs += games
+    def backpropagate(self, winner):
+        if winner == self.player_num:
+            self.num_wins += 1
+        self.num_runs += 1
         if self.parent:
-            self.parent.backpropagate(wins, games)
+            self.parent.backpropagate(winner)
 
     def trial(self, num_games):
-        self.results[self] = self.executor.submit(run_trial, self.state, self.player_num, num_games)
+        results = [self.executor.submit(run_trial, self.state, self.player_num) for i in range(num_games)]
+        for result in results:
+            self.update(result.result())
+
+    def ucb1(self):
+        return self.num_wins / self.num_runs + sqrt(2 * log(self.parent.num_runs) / self.num_runs)
 
     def select(self):
-        if self.busy():
-            self.wait()
-        if len(self.children) < len(self.state.moves(self.player_num)):
+        if len(self.state.moves(self.player_num)) == 0:
+            return None
+        elif len(self.children) < len(self.state.moves(self.player_num)):
             move = list(set(self.state.moves(self.player_num)) - {child.move for child in self.children})[0] # TODO: ordering
             child = MonteCarloTree(move, self, self.depth + 1)
             self.children.append(child)
             return child
         else:
-            options = [(node.num_wins / node.num_runs, node) for node in self.children if not node.all_busy() and node.num_runs > 0]
-            if len(options) == 0:
-                options = [(node.num_wins / node.num_runs, node) for node in self.children if not node.busy() and node.num_runs > 0]
-            if len(options) == 0:
-                options = [(node.num_wins / node.num_runs, node) for node in self.children if node.num_runs > 0]
-            if len(options) == 0:
-                self.children[0].wait()
-                options = [(node.num_wins / node.num_runs, node) for node in self.children if node.num_runs > 0]
-            if self.player_num == 1:
-                return max(options)[1].select()
-            elif self.player_num == 2:
-                return min(options)[1].select()
+            return max([(node.ucb1(), node) for node in self.children])[1].select()
 
     def get_move(self):
-        #print(self)
-        if self.player_num == 1:
-            score, node = max((node.num_wins / node.num_runs, node) for node in self.children)
-        elif self.player_num == 2:
-            score, node = min((node.num_wins / node.num_runs, node) for node in self.children)
+        score, node = max((node.num_wins / node.num_runs, node) for node in self.children)
         print("Score:", score)
         return node.move
 
@@ -155,10 +120,16 @@ class MonteCarloSearchPlayer(Player):
 
     def __init__(self, timeout = 10, num = None):
         self.timeout = timeout
+        self.tree = None
         super().__init__(num)
         
     def get_move(self, board, verbose = False):
-        tree = MonteCarloTree(board, player_num = self.num)
-        tree.run_trials(self.timeout)
-        #print(tree)
-        return tree.get_move()
+        #if self.tree:
+        #    child_boards = {child.state: child for child in self.tree.children}
+        #    if board in child_boards and self.num == child_boards[board].player_num:
+        #        tree = child_boards[board].player_num
+        #if not self.tree:
+        self.tree = MonteCarloTree(board, player_num = self.num)
+        self.tree.run_trials(self.timeout)
+        #print(self.tree)
+        return self.tree.get_move()
